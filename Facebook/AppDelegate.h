ab113510610424d877a8c0e0626cfe1cba35022d
khaled el morabea <khaledelmorabea@khaleds-Mac-mini.local>
2015-02-04 08:25:47 +0200

    Initial Commit

---
+++ Facebook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Facebook
+//
+//  Created by khaled el morabea on 2/4/15.
+//  Copyright (c) 2015 Ibtikar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Facebook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Facebook
+//
+//  Created by khaled el morabea on 2/4/15.
+//  Copyright (c) 2015 Ibtikar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
